69d1a55287d7727593869424c3691d9aa2c59fd5
Darioazzali <da.azzali@gmail.com>
2025-07-22 16:03:16 +0200

    removed realloc

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dazzali <dazzali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 15:41:54 by dazzali           #+#    #+#             */
-/*   Updated: 2025/06/19 12:36:47 by aluque-v         ###   ########.fr       */
+/*   Updated: 2025/07/22 16:03:03 by dazzali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, int n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_realloc(void *ptr, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *haystack, int needle);
