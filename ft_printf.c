09098e8caa253cf7339e56e4eb6d94fded8b7d27
Darioazzali <da.azzali@gmail.com>
2025-07-28 11:46:13 +0200

    variadic error

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dazzali <dazzali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 10:16:08 by dazzali           #+#    #+#             */
-/*   Updated: 2025/07/28 11:40:58 by dazzali          ###   ########.fr       */
+/*   Updated: 2025/07/28 11:46:04 by dazzali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,30 @@ static void	_init_sm(t_print_sm *sm, const char *fmt);
 
 int	ft_printf(const char *fmt, ...)
 {
-	return (ft_fprintf(1, fmt));
+	va_list		args;
+	t_print_sm	sm;
+	int			res;
+
+	if (!fmt)
+		return (-1);
+	va_start(args, fmt);
+	_init_sm(&sm, fmt);
+	while (*sm.fmt != '\0')
+	{
+		if (*sm.fmt == '%')
+		{
+			res = ft_print_cursor(1, &sm, args);
+			if (res == -1)
+				return (-1);
+		}
+		else
+			++(sm.fmt);
+	}
+	res = write(1, sm.cursor, sm.fmt - sm.cursor);
+	if (res == -1)
+		return (-1);
+	va_end(args);
+	return (sm.printed + res);
 }
 
 int	ft_fprintf(int fd, const char *fmt, ...)
